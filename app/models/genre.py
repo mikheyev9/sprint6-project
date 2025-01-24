e2c0504df07ac2f4bfd72039bbf65ee340f9d656
Alexey <theivlev@yandex.ru>
2025-01-24 09:54:52 +0400

    Последняя версия

--- app/models/genre.py
+++ app/models/genre.py
@@ -5,3 +5,8 @@ from .mixins import UUIDMixin
 class GenreDTO(UUIDMixin):
     """Модель жанров"""
     name: str = Field(default_factory=str)
+
+
+# Через obj connect запрашиваем эластик(логика с redis) и 
+# получая данные мы выладируем их с помощью pydantic 
+# и отдаем response
