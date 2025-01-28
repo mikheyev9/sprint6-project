90f0361aabd738c1c7074340635602583f523ea0
mikheyev9 <mikheyev9@gmail.com>
2025-01-28 04:04:01 +0300

    feat: Add caching and update service dependencies in API routers

--- fastapi/src/api/v1/films.py
+++ fastapi/src/api/v1/films.py
@@ -1,38 +1,29 @@
 from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Request, Response
 
 
-from src.models.film import MovieInfoDTO, MovieBaseDTO
-from src.services.film import FilmService, get_film_service
+from models.film import MovieInfoDTO, MovieBaseDTO
+from services.film import FilmService
+from services.service_factory import service_for
+from fastapi_cache.decorator import cache
 
 router = APIRouter()
 
 
-
-@router.get('/{film_id}', response_model=MovieInfoDTO)
-async def film_details(
-    film_id: str,
-    film_service: FilmService = Depends(get_film_service)
-) -> MovieInfoDTO:
-    film = await film_service.get_by_id(film_id)
-    if not film:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='film not found'
-        )
-    return film
-
-
+@cache(expire=60)
 @router.get('/', response_model=list[MovieBaseDTO])
 async def get_films(
+    request: Request,
+    response: Response, 
     genre: str | None = None,
     page_size: Annotated[int, Query(gt=0)] = 50,
     page_number: Annotated[int, Query(gt=0)] = 1,
     sort: Annotated[str, Query()] = 'imdb_rating',
-    film_service: FilmService = Depends(get_film_service)
+    film_service: FilmService = Depends(service_for("film"))
  ) -> list[MovieBaseDTO]:
-    films = await film_service.get_films(
+    films = await film_service.search(
         genre=genre,
         page_number=page_number,
         page_size=page_size,
@@ -45,14 +36,34 @@ async def get_films(
     return films
 
 
+@cache(expire=60, )
+@router.get('/{film_id}', response_model=MovieInfoDTO)
+async def film_details(
+    request: Request,
+    response: Response, 
+    film_id: str,
+    film_service: FilmService = Depends(service_for("film"))
+) -> MovieInfoDTO:
+    film = await film_service.get_by_id(film_id)
+    if not film:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='film not found'
+        )
+    return film
+
+
+
+@cache(expire=60)
 @router.get('/search/', response_model=list[MovieBaseDTO])
 async def search_films(
+    request: Request,
+    response: Response, 
     page_size: Annotated[int, Query(gt=0)] = 50,
     page_number: Annotated[int, Query(gt=0)] = 1,
     query: Annotated[str, Query()] = None,
-    film_service: FilmService = Depends(get_film_service)
+    film_service: FilmService = Depends(service_for("film"))
 ) -> list[MovieBaseDTO]:
-    films = await film_service.get_films(
+    films = await film_service.search(
         page_number=page_number,
         page_size=page_size,
         query=query,
