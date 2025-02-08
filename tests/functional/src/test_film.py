4dbd3443bdd230931366366f28b9cbb67a845d50
Alexey <theivlev@yandex.ru>
2025-02-08 11:19:52 +0400

    Подготовил данные movies, title, rating, genre для test_film

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -4,3 +4,11 @@ from uuid import uuid4 # noqa
 import pytest # noqa
 from tests.functional.settings import test_settings # noqa
 from tests.functional.testdata.es_generate.film_generate import MOVIES_DATA # noqa
+
+MOVIES: list[dict] = MOVIES_DATA.get('movies')
+
+MOVIES_TITLE: list[str] = list(map(lambda movie: movie['title'], MOVIES))
+MOVIES_IMDB_RATING: list[str] = list(
+    map(lambda movie: movie['imdb_rating'], MOVIES)
+)
+MOVIES_GENRE: list[dict] = list(map(lambda movie: movie['genre']))
