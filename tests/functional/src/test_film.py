af55b3d2c2e4b8de80eed86cc226fa39db79ff3a
Alexey <theivlev@yandex.ru>
2025-02-09 18:40:16 +0400

    Предворительные тесты

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,171 +1,179 @@
-import logging
 from http import HTTPStatus
 from uuid import uuid4
 import pytest
 from functional.testdata.es_generate.film_generate import MOVIES_DATA
-from uuid import UUID
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
-logger = logging.getLogger(__name__)
 
 MOVIES_URL = 'films'
-MOVIES: list[dict] = MOVIES_DATA.get('movies')
+MOVIES = MOVIES_DATA.get('movies')
 
-MOVIES_TITLE: list[str] = list(map(lambda movie: movie['title'], MOVIES))
-
-MOVIES_IMDB_RATING: list[str] = list(
-    map(lambda movie: movie['imdb_rating'],
-        MOVIES)
-)
+MOVIES_TITLE = [movie['title'] for movie in MOVIES]
+MOVIES_IMDB_RATING = [movie['imdb_rating'] for movie in MOVIES]
 
 
-@pytest.mark.parametrize("movie_id", [None, "invalid_id", 99999])
+@pytest.mark.parametrize("movie_id", [None, "недействительный_id", 99999])
 async def test_film_validation(make_get_request, movie_id):
-    logger.info(f"Тестирование валидации фильма для movie_id: {movie_id}")
-    response_status, _, _ = await make_get_request(
-        MOVIES_URL,
-        {'id': movie_id} if movie_id else None
+    """Тестирование валидации фильма для различных входных значений."""
+
+    url = (
+        f"{MOVIES_URL}/{movie_id}"
+        if movie_id is not None
+        else MOVIES_URL
     )
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND
+    response_status, response_data, _ = await make_get_request(url)
+
+    if movie_id is None:
+        assert response_status == HTTPStatus.OK, (
+            "Ожидали статус OK, когда movie_id не предоставлен."
+        )
+        assert isinstance(response_data, list), (
+            "Ожидали список фильмов в ответе."
+        )
+    else:
+        assert response_status == HTTPStatus.NOT_FOUND, (
+            "Ожидали статус NOT_FOUND для недействительного movie_id."
+        )
+        expected_error = {"detail": "film not found"}
+        assert response_data == expected_error, (
+            f"Ожидали {expected_error} в ответе, получили {response_data}."
+        )
 
 
 async def test_film_search_existing_id(make_get_request):
+    """ Тестирование поиска существующего фильма по его ID. """
     existing_movie = MOVIES[0]
-    logger.info(f"Поиск существующего фильма по ID: {existing_movie['id']}")
 
     response_status, json_response, _ = await make_get_request(
-        MOVIES_URL + "/" + existing_movie["id"]),
-    logger.info(f'URL запроса: {MOVIES_URL + "/" + existing_movie["id"]}')
-    logger.info(f"Статус ответа: {response_status}, Тело ответа: {json_response}")
-    assert response_status == HTTPStatus.OK
-    assert json_response == existing_movie
+        f"{MOVIES_URL}/{existing_movie['id']}"
+    )
+
+    assert response_status == HTTPStatus.OK, (
+        "Ожидали статус OK при поиске существующего фильма."
+    )
+    assert json_response['id'] == existing_movie['id'], (
+        "Ожидали совпадение ID фильма."
+    )
+    assert json_response['title'] == existing_movie['title'], (
+        "Ожидали совпадение названия фильма."
+    )
 
 
 async def test_film_search_existing_title(make_get_request):
+    """ Тестирование поиска существующего фильма по заголовку. """
     existing_title = MOVIES_TITLE[0]
-    logger.info(f"Поиск существующего фильма по заголовку: {existing_title}")
     response_status, json_response, _ = await make_get_request(
-        MOVIES_URL,
-        {'title': existing_title}
+        MOVIES_URL, {'title': existing_title}
     )
-    logger.info(f"Статус ответа: {response_status}, Тело ответа: {json_response}")
-    assert response_status == HTTPStatus.OK
-    assert json_response['title'] == existing_title
 
-
-async def test_film_search_nonexistent_title(make_get_request):
-    title = 'Несуществующий заголовок фильма'
-    logger.info(f"Поиск несущетствующего заголовка фильма: {title}")
-    response_status, _, _ = await make_get_request(
-        MOVIES_URL,
-        {'title': title}
+    assert response_status == HTTPStatus.OK, (
+        "Ожидали статус OK при поиске существующего заголовка."
     )
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND
+    assert any(movie['title'] == existing_title for movie in json_response), (
+        f"Фильм с заголовком '{existing_title}' не найден."
+    )
 
 
 async def test_get_all_movies(make_get_request):
-    logger.info("Получение всех фильмов")
+    """ Тестирование получения всех фильмов. """
     response_status, json_response, _ = await make_get_request(MOVIES_URL)
-    logger.info(f"Статус ответа: {response_status}, Тело ответа: {json_response}")
-    assert response_status == HTTPStatus.OK
-    assert len(json_response) == len(MOVIES[:50])
+
+    assert response_status == HTTPStatus.OK, (
+        "Ожидали статус OK при получении всех фильмов."
+    )
+    assert len(json_response) == len(MOVIES[:50]), (
+        "Количество возвращаемых фильмов не совпадает с длиной набора данных.")
 
 
 async def test_cache_behavior(make_get_request, redis_client):
+    """ Тестирование кэша при одинаковм запросе. """
     existing_movie = MOVIES[0]
-    logger.info(f"Тестирование поведения кэша для ID фильма: {existing_movie['id']}")
 
-    response_first_status, json_response_first, _ = await make_get_request(
-        MOVIES_URL + "/" + UUID(existing_movie["id"]),
+    response_first_status, json_response_first, timestamp = (
+        await make_get_request(
+            f"{MOVIES_URL}/{existing_movie['id']}")
+        )
+
+    assert response_first_status == HTTPStatus.OK, (
+        "Ожидали статус OK для первого запроса."
     )
-    logger.info(f"Первый статус ответа: {response_first_status}, Тело ответа: {json_response_first}")
-    assert response_first_status == HTTPStatus.OK
-    assert json_response_first == existing_movie
-
-    response_second_status, json_response_second, _ = await make_get_request(
-        MOVIES_URL,
-        {'id': UUID(existing_movie["id"])}
+    assert json_response_first['id'] == existing_movie['id'], (
+        "Ожидали совпадение ID фильма."
     )
-    logger.info(f"Второй статус ответа: {response_second_status}, Тело ответа: {json_response_second}")
-    assert response_second_status == HTTPStatus.OK
-    assert json_response_second == existing_movie
+    assert json_response_first['title'] == existing_movie['title'], (
+        "Ожидали совпадение названия фильма."
+    )
 
-    cached_movie = await redis_client.get(f'film:{existing_movie["id"]}')
-    logger.info(f"Закэшированный фильм: {cached_movie}")
-    assert cached_movie is not None
+    response_second_status, json_response_second, timestamp_redis = (
+        await make_get_request(
+            f"{MOVIES_URL}/{existing_movie['id']}")
+    )
+    assert timestamp > timestamp_redis, (
+        'Ожидали, что второй запрос вернет данные из кэша Redis, '
+        'но, похоже, он был выполнен напрямую из базы данных.'
+    )
 
 
 @pytest.mark.parametrize("movie_id", [uuid4() for _ in range(5)])
 async def test_film_search_random_ids(make_get_request, movie_id):
-    logger.info(f"Поиск случайного ID фильма: {movie_id}")
+    """ Тестирование поиска фильмов по случайным ID."""
+    """ Которые не должны существовать. """
     response_status, _, _ = await make_get_request(
-        MOVIES_URL,
-        {'id': movie_id}
+        f"{MOVIES_URL}/{movie_id}"
     )
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND
+    assert response_status == HTTPStatus.NOT_FOUND, (
+        "Ожидали статус NOT_FOUND для несуществующих ID фильмов."
+    )
 
 
 @pytest.mark.parametrize(
     'query_data, expected_answer',
     [
-        ({'sort': 'title'}, {
-            'status': HTTPStatus.OK,
-            'body': sorted(MOVIES_TITLE[:50]),
-            'length': 50}),
         ({'sort': 'imdb_rating'}, {
             'status': HTTPStatus.OK,
             'body': sorted(MOVIES_IMDB_RATING[:50]),
             'length': 50}),
+        ({'page': 5},
+         {'status': HTTPStatus.OK, 'body': [], 'length': 0}),
+        ({'page': 1, 'page_size': 3},
+         {'status': HTTPStatus.OK, 'body': sorted(MOVIES_TITLE[:3]),
+          'length': 3}),
     ],
 )
 async def test_movies_sorting_and_filtering(
     make_get_request,
-    query_data,
-    expected_answer
+    query_data, expected_answer
 ):
-    logger.info(f"Тестирование сортировки и фильтрации фильмов с параметрами: {query_data}")
+    """ Тестирование сортировки и фильтрации фильмов с различными params. """
     response_status, json_response, _ = await make_get_request(
-        MOVIES_URL, query_data
-    )
-    logger.info(f"Статус ответа: {response_status}, Тело ответа: {json_response}")
-    assert response_status == expected_answer['status']
-
-
-@pytest.mark.parametrize("movie_id", ["", "invalid_uuid", "12345"])
-async def test_film_validation_edge_cases(make_get_request, movie_id):
-    logger.info(f"Тестирование крайних случаев валидации фильма для movie_id: {movie_id}")
-    response_status, _, _ = await make_get_request(
         MOVIES_URL,
-        {'id': movie_id} if movie_id else None
+        query_data
     )
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND
 
-
-@pytest.mark.parametrize("title", ["", "A" * 10])
-async def test_film_search_title_edge_cases(make_get_request, title):
-    logger.info(f"Тестирование крайних случаев поиска заголовка фильма для заголовка: '{title}'")
-    response_status, _, _ = await make_get_request(
-        MOVIES_URL,
-        {'title': title}
+    assert response_status == expected_answer['status'], (
+        f"Ожидали статус {expected_answer['status']}, "
+        f"получили {response_status}."
     )
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND
 
 
 @pytest.mark.parametrize("query_data", [
-    {'sort': 'invalid_sort'},
-    {'genre': 'invalid_genre'},
+    {'genre': 'недействительный_жанр'},
 ])
 async def test_movies_sorting_and_filtering_invalid_params(
     make_get_request,
     query_data
 ):
-    logger.info(f"Тестирование некорректных параметров сортировки и фильтрации: {query_data}")
+    """ Тестирование поведения при наличии недействительных параметров. """
+
     response_status, _, _ = await make_get_request(MOVIES_URL, query_data)
-    logger.info(f"Статус ответа: {response_status}")
-    assert response_status == HTTPStatus.NOT_FOUND  #  HTTPStatus.UNPROCESSABLE_ENTITY
+
+    if 'sort' in query_data:
+        assert response_status == HTTPStatus.INTERNAL_SERVER_ERROR, (
+            "Ожидали внутреннюю ошибку"
+            "сервера для недействительных параметров сортировки."
+        )
+    elif 'genre' in query_data:
+        assert response_status == HTTPStatus.NOT_FOUND, (
+            'Ожидали статус NOT_FOUND при поиске недействительного жанра.'
+        )
+    else:
+        assert False, 'Неизвестный параметр, не удалось проверить ввод.'
