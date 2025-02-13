7b566bfee37d171ff73832dda5ecf253728fd316
mikheyev9 <mikheyev9@gmail.com>
2025-02-13 20:52:14 +0300

    refactor: Improve configuration handling for Redis and Elasticsearch DSN generation

--- fastapi/src/core/config.py
+++ fastapi/src/core/config.py
@@ -1,4 +1,3 @@
-import os
 from logging import config as logging_config
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -9,12 +8,12 @@ logging_config.dictConfig(LOGGING_CONFIG)
 
 
 class Settings(BaseSettings):
-    #FastAPI
+    # FastAPI
     project_name: str
     project_summary: str
     project_version: str
     project_terms_of_service: str
-    project_tags: list = Field([
+    project_tags: list = Field(default=[
         {
             "name": 'films',
             "description": 'Operations with films.',
@@ -35,12 +34,24 @@ class Settings(BaseSettings):
     redis_user: str
     redis_password: str
     redis_db_index: int
-    redis_dsn: str
+    redis_dsn: str = ""
 
     # Elasticsearch
-    elasticsearch_dsn: str
+    elasticsearch_host: str
+    elasticsearch_port: int
+    elasticsearch_dsn: str = ""
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+    def model_post_init(self, __context):
+        """Формируем DSN после загрузки переменных"""
+        
+        self.redis_dsn = f"redis://{self.redis_user}:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_db_index}"
+        self.elasticsearch_dsn = f"http://{self.elasticsearch_host}:{self.elasticsearch_port}/"
 
 
 settings = Settings()
--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -5,7 +5,7 @@ class TestSettings(BaseSettings):
     # Elasticsearch
     elasticsearch_host: str
     elasticsearch_port: int
-    elasticsearch_dsn: str
+    elasticsearch_dsn: str = ""
 
     # Redis
     redis_host: str
@@ -13,14 +13,21 @@ class TestSettings(BaseSettings):
     redis_user: str
     redis_password: str
     redis_db_index: int
-    redis_dsn: str
+    redis_dsn: str = ""
 
-    # Fastapi
+    # FastAPI
     service_host: str
     service_port: int
-    service_dsn: str
+    service_dsn: str = ""
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=".env")
+
+    def model_post_init(self, __context):
+        """Формируем DSN после загрузки переменных"""
+        
+        self.elasticsearch_dsn = f"http://{self.elasticsearch_host}:{self.elasticsearch_port}/"
+        self.redis_dsn = f"redis://{self.redis_user}:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_db_index}"
+        self.service_dsn = f"http://{self.service_host}:{self.service_port}"
 
 
 test_settings = TestSettings()
