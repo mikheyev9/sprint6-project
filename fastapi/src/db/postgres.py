996dc33bcdc9ce92a5cb40862d4b72ccda59d644
Александр Мишаков <sasha.mishakov@gmail.com>
2025-03-02 20:44:57 +0300

    #61 feat: create role model

--- fastapi/src/db/postgres.py
+++ fastapi/src/db/postgres.py
@@ -1,22 +1,24 @@
+import re
 import uuid
 
 from sqlalchemy import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import Mapped, declarative_base, declared_attr, mapped_column, sessionmaker
-from src.core.config import postgres_settings
+from src.core.config import postgres_settings, project_settings
 
 
 class PreBase:
     @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
+    def __tablename__(self) -> str:
+        """Добавляет название таблицы по названию класса в метаданные модели в стиле snake_case."""
+        return re.sub(r"(?<!^)(?=[A-Z])", "_", self.__name__).lower()
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, sort_order=-1)
 
 
 Base = declarative_base(cls=PreBase)
 
-engine = create_async_engine(postgres_settings.dsn)
+engine = create_async_engine(postgres_settings.dsn, echo=project_settings.debug)
 
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
