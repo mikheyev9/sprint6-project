8eb7a5deeb34c80db20bbb49f51471df35899026
Ivlev Alexey <theivlev@yandex.ru>
2025-03-12 15:58:43 +0400

    Добавил транзакию

--- fastapi_auth/src/models/auth_history.py
+++ fastapi_auth/src/models/auth_history.py
@@ -3,61 +3,77 @@ from uuid import UUID
 
 from sqlalchemy import ForeignKey, UniqueConstraint, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy.exc import SQLAlchemyError
 from src.db.postgres import Base
 from dateutil.relativedelta import relativedelta
+from sqlalchemy.engine import Connection
 
 
-def create_monthly_partitions(connection, table_name, device_type, start_date: datetime, end_date: datetime):
-    while start_date < end_date:
-        partition_name = f"{table_name}_{device_type}_{start_date.strftime('%Y_%m')}"
-        next_month = start_date + relativedelta(month=1)
-        sql = text(
-            f"""
-            CREATE TABLE IF NOT EXISTS "{partition_name}"
-            PARTITION OF "{table_name}"
-            FOR VALUES FROM (:start_date) TO (:next_month)
-            """
-        )
-        connection.execute(
-            sql.bindparams(
-                start_date=start_date.isoformat(),
-                next_month=next_month.isoformat()
+def create_monthly_partitions(
+        connection: Connection,
+        table_name: str,
+        device_type: str,
+        start_date: datetime,
+        end_date: datetime
+) -> None:
+    transaction = connection.begin()
+    try:
+        while start_date < end_date:
+            partition_name = f"{table_name}_{device_type}_{start_date.strftime('%Y_%m')}"
+            next_month = start_date + relativedelta(months=1)
+            sql = text(
+                f"""
+                CREATE TABLE IF NOT EXISTS "{partition_name}"
+                PARTITION OF "{table_name}"
+                FOR VALUES FROM (:start_date) TO (:next_month)
+                """
             )
-        )
-        start_date = next_month
+            connection.execute(
+                sql.bindparams(
+                    start_date=start_date.isoformat(),
+                    next_month=next_month.isoformat()
+                )
+            )
+            start_date = next_month
+        transaction.commit()
+    except SQLAlchemyError:
+        transaction.rollback()
+        raise SQLAlchemyError(f'Ошибка create monthly partitions для таблицы: {table_name}')
 
 
 def create_partition(target, connection, **kw) -> None:
-    """creating partition by user_sign_in"""
+    """Создание партиций по типу устройства и времени"""
     device_types = ['smart', 'mobile', 'web']
     now = datetime.now()
     current_year = now.year
     start_date = datetime(current_year, 1, 1)
     end_date = datetime(current_year + 2, 1, 1)
 
-    for device_type in device_types:
-        partition_table_name = f'auth_history_{device_type}'
-        sql = text(
-            f"""
-            CREATE TABLE IF NOT EXISTS "{partition_table_name}"
-            PARTITION OF "auth_history"
-            FOR VALUES IN (:device_type)
-            PARTITION BY RANGE (timestamp)
-            """
-        )
-        connection.execute(
-            sql.bindparams(
-                device_type=device_type
+    try:
+        for device_type in device_types:
+            partition_table_name = f'auth_history_{device_type}'
+            sql = text(
+                f"""
+                CREATE TABLE IF NOT EXISTS "{partition_table_name}"
+                PARTITION OF "auth_history"
+                FOR VALUES IN (:device_type)
+                PARTITION BY RANGE (timestamp)
+                """
             )
-        )
-        create_monthly_partitions(
-            connection,
-            partition_table_name,
-            device_type,
-            start_date,
-            end_date
-        )
+            connection.execute(
+                sql.bindparams(
+                    device_type=device_type
+                )
+            )
+            create_monthly_partitions(
+                connection,
+                partition_table_name,
+                device_type,
+                start_date,
+                end_date
+            )
+    except SQLAlchemyError:
+        raise
 
 
 class AuthHistory(Base):
@@ -70,7 +86,7 @@ class AuthHistory(Base):
         }
     )
 
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     user_agent: Mapped[str] = mapped_column(nullable=False)
     user_device_type: Mapped[str] = mapped_column(nullable=False, primary_key=True)
     timestamp: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
