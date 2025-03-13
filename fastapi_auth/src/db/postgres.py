c983510331eb764ff18e7132d9b040229da2c65b
Ivlev Alexey <theivlev@yandex.ru>
2025-03-13 11:31:48 +0400

    Добавил инициализацию только одной реплики(без теста)

--- fastapi_auth/src/db/postgres.py
+++ fastapi_auth/src/db/postgres.py
@@ -1,11 +1,13 @@
 import re
 import uuid
+import asyncio
 
 from sqlalchemy import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import Mapped, declarative_base, declared_attr, mapped_column, sessionmaker
 from src.core.config import postgres_settings, project_settings
 from sqlalchemy import MetaData
+from redis import asyncio as aioredis
 
 metadata = MetaData()
 
@@ -26,9 +28,24 @@ engine = create_async_engine(postgres_settings.dsn, echo=project_settings.debug)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
 
-async def create_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+async def create_database(redis_client: aioredis.Redis):
+    lock_key = 'db_init_lock'
+    lock_timeout = 60
+
+    try:
+        lock_acquired = await redis_client.setnx(lock_key, '1')
+        if lock_acquired:
+            await redis_client.expire(lock_key, lock_timeout)
+            try:
+                async with engine.begin() as conn:
+                    await conn.run_sync(Base.metadata.create_all)
+            finally:
+                await redis_client.delete(lock_key)
+        else:
+            while await redis_client.exists(lock_key):
+                await asyncio.sleep(1)
+    except Exception:
+        raise
 
 
 async def get_async_session():
