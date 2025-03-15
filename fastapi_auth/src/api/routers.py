279d3cd43104c6649ea700c8c9a07a65369a14eb
Theivlev <theivlev@yandex.ru>
2025-03-15 15:34:23 +0400

    импорты

--- fastapi_auth/src/api/routers.py
+++ fastapi_auth/src/api/routers.py
@@ -6,4 +6,8 @@ API_V1: str = "/auth/v1"
 main_router = APIRouter()
 main_router.include_router(user_router, prefix=f"{API_V1}")
 main_router.include_router(role_router, prefix=f"{API_V1}/roles", tags=["roles"])
-main_router.include_router(auth_history_router, prefix=f"{API_V1}/auth_history", tags=["auth_history"])
+main_router.include_router(
+    auth_history_router,
+    prefix=f"{API_V1}/auth_history",
+    tags=["auth_history"]
+)
--- fastapi_auth/src/api/v1/__init__.py
+++ fastapi_auth/src/api/v1/__init__.py
@@ -1,3 +1,3 @@
-from .role_api import router as role_router
-from .user_api import router as user_router
-from .auth_history_api import router as auth_history_router
+from .role_api import router as role_router # noqa
+from .user_api import router as user_router # noqa
+from .auth_history_api import router as auth_history_router # noqa
