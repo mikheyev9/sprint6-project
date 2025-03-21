dc100fc818d2d56dae85fd816ec5bbb03527b4f4
Evgeniy_Kudryashov <gagarinru84@yandex.ru>
2025-03-21 19:43:48 +0300

    feat: del flask_app

--- flask_app/views/analytics/views.py
+++
@@ -1,8 +0,0 @@
-# import requests
-
-
-class Analitics:
-    def get_start_servers():
-        """Получения статуса серверов"""
-        # requests.get('http://slow_application_host/slow-operation§')
-        return "Hello, World!"
